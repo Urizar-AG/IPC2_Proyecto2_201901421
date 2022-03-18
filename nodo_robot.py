70ca4223ef64cf6b7cff0d1431f8ed5a4daa876d
Angel <3046108010115@ingenieria.usac.edu.gt>
2022-03-17 23:31:12 -0600

    Corrige el fallo en el método __init__ de la clase Robot

--- nodo_robot.py
+++ nodo_robot.py
@@ -1,21 +1,25 @@
 
 class Robot:
 
-    def init(self, nombre, tipo):
+    def __init__(self, nombre, tipo):
         self.nombre = nombre
-        self.tipo = tipo
+        self.tipo = tipo #ChapinFighter o ChapinRescue
         self.capacidad_combate = 0
         self.next = None
+
+    #Getters y Setters
 
     def getNombre(self):
         return self.nombre
 
     def setNombre(self, nombre):
         self.nombre = nombre    
+    
+    def getTipo(self):
+        return self.tipo
 
     def getCapacidadCombate(self):
         return self.capacidad_combate
 
     def setCapacidadCombate(self, capacidad):
         self.capacidad_combate = capacidad
-        
