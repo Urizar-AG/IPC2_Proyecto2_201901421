dcc149e91018c44e8c092364fb70081cf7bd4b71
Angel <3046108010115@ingenieria.usac.edu.gt>
2022-03-20 01:45:29 -0600

    Agrega la clase ListaCiudades y sus métodos

---
+++ lista_ciudad.py
@@ -0,0 +1,65 @@
+from nodo_ciudad import Ciudad
+
+class ListaCiudades():
+
+    def __init__(self):
+        self.size = 0
+        self.head = None
+
+    #Añade al final
+    def addEnd(self, nombre, filas, columnas):
+        nuevo = Ciudad(nombre, filas, columnas)
+        self.size += 1
+        if self.head is None:
+            self.head = nuevo
+        else:
+            tmp = self.head
+            while tmp.next is not None:
+                tmp = tmp.next
+            tmp.next = nuevo
+
+    #Busca una ciudad por su nombre y tipo, si encuentra coincidencia retorna el nodo, caso contrario retorna None
+    def searchCiudad(self, nombre):
+        tmp = self.head
+        while tmp is not None:
+            if tmp.getNombre() == nombre:
+                return tmp
+            tmp = tmp.next
+        return None 
+    
+    #Elimina el nodo de la ciudad que recibe por parámetro
+    def deleteCiudad(self, nombre):
+        tmp = self.head
+        while tmp is not None:
+            if tmp.getNombre() == nombre:
+                #Si el nodo a eliminar es el primero
+                if tmp == self.head:
+                    self.head = tmp.next
+                    tmp.next = None
+                #Si el nodo a eliminar es el último
+                elif tmp.next is None:
+                    anterior = self.head #Nodo anterior al nodo actual(el nodo actual es el que se está borrando)
+                    while anterior.next.getNombre() != tmp.getNombre():
+                        anterior = anterior.next
+                    anterior.next = None
+                #Si no es el primer nodo o el último nodo
+                else:
+                    siguiente = tmp.next #Nodo siguiente al nodo actual
+                    anterior = self.head #Nodo anterior al nodo actual
+                    while anterior.next.getNombre() != tmp.getNombre():
+                        anterior = anterior.next
+                    anterior.next = siguiente #El nodo anterior a actual ahora apunta al nodo siguiente del actual.
+                    tmp.next = None
+            tmp = tmp.next
+
+    #Devuelve el tamaño de la lista
+    def getSize(self):
+        return self.size
+    
+    #Verifica si la lista está o no vacía
+    #True si está vacía, False caso contrario
+    def isEmpty(self):
+        if self.size == 0:
+            return True
+        else:
+            return False
